e4befe395afdbd1aae7e5aa9cf52920990a284f2
Khang Vo <KhangVoT@gmail.com>
2022-07-05 21:48:22 -0500

    update

--- test.py
+++ test.py
@@ -1,30 +1,61 @@
-"""in the works script to interpolate global velocity model to become more coarse"""
-
 import os
-import glob
-import time
-
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.interpolate import griddata
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import interpn
 
-ak135_file = "/Users/khangvo/Python_Projects/Puerto_Rico_Project/files/_v_list/ak135.txt"
-mit_file = "/Users/khangvo/Python_Projects/Puerto_Rico_Project/files/_v_list/ggge1202-sup-0002-ds01.txt"
 
-df = pd.read_csv(mit_file, delim_whitespace=True)
+def create_model(mit_file):
+    df = pd.read_csv(mit_file, delim_whitespace=True)
 
-points = df[["Lat", "Long", "Depth"]].to_numpy()
-values = np.array(df["dVp"])
+    # Extract the list of coordinates
+    xs = np.array(df["Lat"].to_list())
+    ys = np.array(df["Long"].to_list())
+    zs = np.array(df["Depth"].to_list())
+    # Extract the associated velocity values
+    vs = np.array(df["dVp"].to_list())
 
-request = np.array([[50, 50, 100], [100, 100, 150]])
+    px, ix = np.unique(xs, return_inverse=True)
+    py, iy = np.unique(ys, return_inverse=True)
+    pz, iz = np.unique(zs, return_inverse=True)
 
-print(griddata(points, values, request))
+    points = (px, py, pz)
 
-# ak135 = pd.read_csv(ak135_file, delim_whitespace=True, header=None, skiprows=1)
-#
-# depth = ak135.iloc[:, 0]
-# velP = ak135.iloc[:, 1]
-# vel_new = np.interp(depth_unq, depth, velP)
-# vel_new = vel_new.round(4)
+    values = np.empty_like(vs, shape=(px.size, py.size, pz.size))
+    values[ix, iy, iz] = vs
+
+    return points, values
+
+
+def interp(points, values):
+
+    # Create desired coordinates
+    lat_request = np.arange(-90, 90 + 1, 5)
+    long_request = np.arange(-180, 180 + 1, 5)
+    depth_request = np.arange(25, 1825 + 1, 100)
+
+    px, ix = np.unique(lat_request, return_inverse=True)
+    py, iy = np.unique(long_request, return_inverse=True)
+    pz, iz = np.unique(depth_request, return_inverse=True)
+
+    # request = (px, py, pz)
+    # print(request)
+
+    # test
+    request = np.array([-40, 0, 25])
+
+    output = interpn(points, values, request, method="linear", bounds_error=False, fill_value=None)
+
+    return output
+
+
+def main():
+    mit_file = "/Users/khangvo/Python_Projects/Puerto_Rico_Project/files/_v_list/ggge1202-sup-0002-ds01.txt"
+
+    points, values = create_model(mit_file)
+
+    output = interp(points, values)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
