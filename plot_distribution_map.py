568e638b0957f92a8cc7ed0dfb1e7ea3355fa85a
Khang Vo <KhangVoT@gmail.com>
2022-10-05 21:39:22 -0400

    update

--- plot_distribution_map.py
+++ plot_distribution_map.py
@@ -1,7 +1,7 @@
 # File Name: plot_distribution_maps
 # Author: Khang Vo
 # Date Created: 9/21/2022
-# Date Last Modified: 9/21/2022
+# Date Last Modified: 10/5/2022
 # Python Version: 3.9
 
 import os
@@ -16,13 +16,21 @@ from scipy.interpolate import Rbf
 from mpl_toolkits.basemap import Basemap
 
 
-def plot_reg(df, ax, n):
+def main(events_reg, events_glb, lon_min, lon_max, lat_min, lat_max):
+    # create main plot
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(19, 9), constrained_layout=True)
+    # fig.suptitle("Distribution Map", fontsize=18, y=0.995)
 
-    plt.subplots()
+    # plot regional events
+    df_reg = pd.read_csv(events_reg, delim_whitespace=True)
+    df_reg.columns = ["date", "time", "glat", "glon", "depth", "mb", "n1", "n2", "n3", "ievt", "n4"]
+    df_reg = df_reg.drop(["n1", "n2", "n3", "n4"], axis=1)
+    df_reg = df_reg.astype(float)
+    df_reg = df_reg[(df_reg["glon"].astype(float) >= lon_min) & (df_reg["glon"].astype(float) <= lon_max) &
+                    (df_reg["glat"].astype(float) >= lat_min) & (df_reg["glat"].astype(float) <= lat_max)]
 
-    # m = Basemap(projection="lcc", resolution="h", lat_0=15, lon_0=-67.5, width=1E6, height=1.2E6)
-    m = Basemap(resolution="h", llcrnrlat=df["glat"].min(), llcrnrlon=df["glon"].min(),
-                urcrnrlat=df["glat"].max(), urcrnrlon=df["glon"].max())
+    m = Basemap(resolution="h", llcrnrlat=df_reg["glat"].min(), llcrnrlon=df_reg["glon"].min(),
+                urcrnrlat=df_reg["glat"].max(), urcrnrlon=df_reg["glon"].max(), ax=ax[0])
     m.drawparallels(np.arange(-90, 90, 10), labels=[1, 0, 0, 0], linewidth=0, xoffset=0.5, yoffset=0.5)
     m.drawmeridians(np.arange(0, 360, 10), labels=[0, 0, 0, 1], linewidth=0, xoffset=0.5, yoffset=0.5)
     m.shadedrelief(scale=0.5)
@@ -30,52 +38,39 @@ def plot_reg(df, ax, n):
     m.drawcountries(color="lightgray")
     m.drawstates(color="lightgray")
 
-    m.scatter(df["glon"], df["glat"], latlon=True, c=df["depth"], s=df["mb"], cmap="jet", alpha=0.5)
+    cl = m.scatter(df_reg["glon"], df_reg["glat"], latlon=True, c=df_reg["depth"], s=df_reg["mb"], cmap="jet", alpha=0.5)
 
-    plt.colorbar(label="Depth (km)")
-    plt.clim(min(df["depth"]), max(df["depth"]))
+    cb = plt.colorbar(cl, ax=ax[0])
+    cb.set_label("Depth (km)")
 
-    for a in [1, 5, 9]:
-        plt.scatter([], [], c="k", alpha=0.5, s=a,
-                    label="Magnitude: " + str(a))
-    plt.legend(scatterpoints=1, frameon=True,
-               labelspacing=1, loc="upper right")
+    ax[0].set_title("Regional Data")
 
-    plt.show()
+    # plot global events
+    df_glb = pd.read_csv(events_glb, delim_whitespace=True)
+    df_glb.columns = ["date", "time", "glat", "glon", "depth", "mb", "n1", "n2", "n3", "ievt", "n4"]
+    df_glb = df_glb.drop(["n1", "n2", "n3", "n4"], axis=1)
+    df_glb = df_glb.astype(float)
 
-
-def plot_glb(df, ax, n):
-
-    plt.subplots()
-
-    m = Basemap(projection="ortho", resolution=None, lat_0=15, lon_0=-67.5)
+    m = Basemap(projection="ortho", resolution=None, lat_0=15, lon_0=-67.5, ax=ax[1])
     m.shadedrelief(scale=0.5)
 
-    m.scatter(df["glon"], df["glat"], latlon=True, c=df["depth"], s=df["mb"], cmap="jet", alpha=0.5)
+    cl = m.scatter(df_glb["glon"], df_glb["glat"], latlon=True, c=df_glb["depth"], s=df_glb["mb"], cmap="jet", alpha=0.5)
 
-    plt.colorbar(label="Depth (km)")
-    plt.clim(min(df["depth"]), max(df["depth"]))
+    cb = plt.colorbar(cl, ax=ax[1])
+    cb.set_label("Depth (km)")
 
-    for a in [1, 5, 9]:
-        plt.scatter([], [], c="k", alpha=0.5, s=a,
-                    label="Magnitude: " + str(a))
-    plt.legend(scatterpoints=1, frameon=True,
-               labelspacing=1, loc="upper right")
+    ax[1].set_title("Global Data")
 
     plt.show()
 
 
-def main(file):
-    # create main plot
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(19, 9), constrained_layout=True)
-    fig.suptitle("Distribution Map", fontsize=18, y=0.995)
+# run main()
+if __name__ == "__main__":
+    # regional events
+    events_reg = "/Users/khangvo/Python_Projects/Puerto_Rico_Project/files/05_TeletomoDD_files/event.txt"
 
-    df = pd.read_csv(file, delim_whitespace=True)
-    df.columns = ["date", "time", "glat", "glon", "depth", "mb", "n1", "n2", "n3", "ievt", "n4"]
-    df = df.drop(["n1", "n2", "n3", "n4"], axis=1)
-    df = df.astype(float)
-
-    plot_glb(df, ax, 0)
+    # global events
+    events_glb = "/Users/khangvo/Python_Projects/Puerto_Rico_Project_Refinement/files/05_TeletomoDD_files/event.txt"
 
     # user specified study area data extent
     lon_min = -80
@@ -83,17 +78,4 @@ def main(file):
     lat_min = 5
     lat_max = 25
 
-    df_zoned = df[(df["glon"].astype(float) >= lon_min) & (df["glon"].astype(float) <= lon_max) &
-                  (df["glat"].astype(float) >= lat_min) & (df["glat"].astype(float) <= lat_max)]
-
-    # plot_reg(df_zoned, ax, 1)
-
-    # plt.show()
-
-
-# run main()
-if __name__ == "__main__":
-
-    file = "/Users/khangvo/Python_Projects/Puerto_Rico_Project_Refinement/files/05_TeletomoDD_files/event.txt"
-
-    main(file)
+    main(events_reg, events_glb, lon_min, lon_max, lat_min, lat_max)
--- plot_histogram.py
+++ plot_histogram.py
@@ -1,7 +1,7 @@
 # File Name: plot_histograms
 # Author: Khang Vo
 # Date Created: 8/13/2022
-# Date Last Modified: 9/24/2022
+# Date Last Modified: 10/5/2022
 # Python Version: 3.9
 
 import os
@@ -13,52 +13,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def plot_glb(df, ax):
-    df["depth"].hist(ax=ax[1], bins=50, color="red", alpha=0.5, histtype="bar", edgecolor="black")
+def main(events_reg, events_glb, lon_min, lon_max, lat_min, lat_max):
+    # create main plot
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(19, 9), constrained_layout=True)
+    # fig.suptitle("Histograms", fontsize=18, y=0.995)
 
-    ax[1].set_title("Global Data")
-    ax[1].set_xlabel("Depth (km)")
-    ax[1].set_ylabel("Number of Earthquakes")
+    # plot regional events
+    df_reg = pd.read_csv(events_reg, delim_whitespace=True)
+    df_reg.columns = ["date", "time", "glat", "glon", "depth", "mb", "n1", "n2", "n3", "ievt", "n4"]
+    df_reg = df_reg[(df_reg["glon"].astype(float) >= lon_min) & (df_reg["glon"].astype(float) <= lon_max) &
+                    (df_reg["glat"].astype(float) >= lat_min) & (df_reg["glat"].astype(float) <= lat_max)]
 
-
-def plot_reg(df, ax, lon_min, lon_max, lat_min, lat_max):
-
-    df = df[(df["glon"].astype(float) >= lon_min) & (df["glon"].astype(float) <= lon_max) &
-            (df["glat"].astype(float) >= lat_min) & (df["glat"].astype(float) <= lat_max)]
-
-    df["depth"].hist(ax=ax[0], bins=50, color="red", alpha=0.5, histtype="bar", edgecolor="black")
+    df_reg["depth"].hist(ax=ax[0], bins=50, color="red", alpha=0.5, histtype="bar", edgecolor="black")
 
     ax[0].set_title("Regional Data")
     ax[0].set_xlabel("Depth (km)")
     ax[0].set_ylabel("Number of Earthquakes")
 
+    # plot global events
+    df_glb = pd.read_csv(events_glb, delim_whitespace=True)
+    df_glb.columns = ["date", "time", "glat", "glon", "depth", "mb", "n1", "n2", "n3", "ievt", "n4"]
 
-def main(event_file, lon_min, lon_max, lat_min, lat_max):
-    # create main plot
-    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(19, 9), constrained_layout=True)
-    # fig.suptitle("Histograms", fontsize=18, y=0.995)
+    df_glb["depth"].hist(ax=ax[1], bins=50, color="red", alpha=0.5, histtype="bar", edgecolor="black")
 
-    df = pd.read_csv(event_file, sep="\t", header=None)
-    df.columns = ["date", "time", "glat", "glon", "depth", "mb", "n1", "n2", "n3", "ievt", "n4"]
-
-    plot_reg(df, ax, lon_min, lon_max, lat_min, lat_max)
-    plot_glb(df, ax)
+    ax[1].set_title("Global Data")
+    ax[1].set_xlabel("Depth (km)")
+    ax[1].set_ylabel("Number of Earthquakes")
 
     plt.show()
 
 
 # run main()
 if __name__ == "__main__":
+    # regional events
+    events_reg = "/Users/khangvo/Python_Projects/Puerto_Rico_Project/files/05_TeletomoDD_files/event.txt"
 
-    # user specified working directory
-    event_file = "/Users/khangvo/Python_Projects/Puerto_Rico_Project/files/05_TeletomoDD_files/event.txt"
+    # global events
+    events_glb = "/Users/khangvo/Python_Projects/Puerto_Rico_Project_Refinement/files/05_TeletomoDD_files/event.txt"
 
     # user specified study area data extent
     lon_min = -80
     lon_max = -55
     lat_min = 5
     lat_max = 25
-    depth_min = 10
-    depth_max = 250
 
-    main(event_file, lon_min, lon_max, lat_min, lat_max)
+    main(events_reg, events_glb, lon_min, lon_max, lat_min, lat_max)
